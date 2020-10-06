06741012f3dc3e9e120c611c40a0ecff295c0d66
andgrabo@tut.by <261090Samsung>
2020-10-06 16:34:41 +0300

    Hi

--- src/test/java/com/incytes/clinician/ClinicianR22A.java
+++ src/test/java/com/incytes/clinician/ClinicianR22A.java
@@ -10,24 +10,24 @@ import static com.codeborne.selenide.Selenide.switchTo;
 public class ClinicianR22A {
     @Test
     public void c_test() {
-            org.selenide.examples.Clinician.Main main = new org.selenide.examples.Clinician.Main("En");
-            org.selenide.examples.Clinician.Main.FileTXT file = main.new FileTXT("D:\\Path\\count.txt");
+            Main main = new Main("En");
+            Main.FileTXT file = main.new FileTXT("D:\\Path\\count.txt");
             int count = Integer.parseInt(file.getText());
-            org.selenide.examples.Clinician.Main.Login login = main.new Login();
+            Main.Login login = main.new Login();
             login.open();
             login.setAll("qwertyuiop17091709+1@yandex.by", "261090inCytes!").wLogin().signIn();
-            org.selenide.examples.Clinician.Main.Login.DashBoard dashBoard = login.new DashBoard();
+            Main.Login.DashBoard dashBoard = login.new DashBoard();
             dashBoard.clickProfile();
-            org.selenide.examples.Clinician.Main.Login.DashBoard.Profile profile = dashBoard.new Profile();
+            Main.Login.DashBoard.Profile profile = dashBoard.new Profile();
             profile.clickInvite();
             profile.writeEmail("qwertyuiop17091709+" + count + "@yandex.by");
             profile.checkingInvitationForm().clickSendInvitation().isInvitationSent();
-            org.selenide.examples.Clinician.Main.newTab();
+            Main.newTab();
             switchTo().window(1);
-            org.selenide.examples.Clinician.Main.GetInvitationWithYandex yandex = main.new GetInvitationWithYandex("qwertyuiop17091709@yandex.ru", "cilaCILA17097938", "+375298746833");
+            Main.GetInvitationWithYandex yandex = main.new GetInvitationWithYandex("qwertyuiop17091709@yandex.ru", "cilaCILA17097938", "+375298746833");
             yandex.clickInvitation();
             switchTo().window(2);
-            org.selenide.examples.Clinician.Main.Registration reg = main.new Registration();
+            Main.Registration reg = main.new Registration();
             reg.setAll("Anton", "Sharifov", "", "261090inCytes!", "261090inCytes!").wRegistration().cRegistration();
             reg.clickNext();
             file.writeText(String.valueOf(count + 1), false);
