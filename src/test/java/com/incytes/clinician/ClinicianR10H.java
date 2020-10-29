a34cf7f2cf22f897d36afe64adaa6806ca948607
John Doe <john@sample.com>
2020-10-29 13:09:20 +0300

    NC

--- src/test/java/com/incytes/clinician/ClinicianR10H.java
+++ src/test/java/com/incytes/clinician/ClinicianR10H.java
@@ -7,16 +7,16 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.switchTo;
 
-public class ClinicianR10H_ERROR {
+public class ClinicianR10H {
     @Test
     public void method() {
-        Main main = new Main("Ge");
+        Main main = new Main("De");
         Main.Registration reg = main.new Registration();
         reg.open();
         reg.clickTerms();
         switchTo().window(1);
         Main.Registration.TermsAndConditions terms = reg.new TermsAndConditions();
-        terms.eHeading().shouldHave(Condition.exactText("Termini e Condizioni"));
+        terms.eHeading().shouldHave(Condition.exactText("Geschäftsbedingungen"));
         terms.firstText().shouldHave(text("™"));
         close();
     }
