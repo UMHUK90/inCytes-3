1904ee3bdd969d170ae5de704909420c8480f90b
Andrew Grabovskiy <agrabovskiy@rgnmed.com>
2020-11-26 15:25:33 +0300

    NC

--- src/test/java/com/incytes/clinician/ClinicianCS094O.java
+++ src/test/java/com/incytes/clinician/ClinicianCS094O.java
@@ -18,7 +18,7 @@ public class ClinicianCS094O {
         cases.clickAddCase();
         Main.Login.DashBoard.Cases.NewCase newCase = cases.new NewCase();
         newCase.clickUseExistingPatient();
-        newCase.setAll("qwertyuiop17091709+243@yandex.by", "", "", "", "", "", "Circle for 1").writeAll().checkAll();
+        newCase.setAll("qwertyuiop17091709+243@yandex.by", "", "", "", "", "", "missing readonly-content").writeAll().checkAll();
         newCase.clickCreateCase();
         $(byText("No available cases for this Circle. Visit the Circle details page to add more cases.")).shouldBe(Condition.visible);
     }
